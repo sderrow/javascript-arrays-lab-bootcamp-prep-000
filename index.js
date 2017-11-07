5c5c7b85244575833791142f4537c628be0afccc
Sean Derrow <sderrow@mit.edu>
2017-11-07 05:57:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const app = "I don't do much."
-kittens = ["Milo", "Otis", "Garfield"]
+var kittens = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendKitten(name) {
-  
+  kittens.push(name)
+  return kittens
 }
