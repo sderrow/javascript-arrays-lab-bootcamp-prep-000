d7b93016c6af809cc5a6f6631925a96dd4ce561b
Sean Derrow <sderrow@mit.edu>
2017-11-07 05:59:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,12 @@ function destructivelyPrependKitten(name) {
   return kittens
 }
 
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
